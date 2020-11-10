6be888b44892abc6985fbedd27b3ba46f43618a7
kang da sol <dskang070@naver.com>
2020-11-10 17:17:07 +0900

    practice_20201110

---
+++ algorithm study/self study/daily/20201110.cpp
@@ -0,0 +1,35 @@
+/*
+1. STL
+2. BFS/DFS
+3. BINARY SEARCH
+4. BINARY TREE
+5. DP
+6. DIJKSTRA
+7. FLOYD_WARSHALL
+*/
+
+#include<iostream>
+#include<queue>
+
+using namespace std;
+int main(void) {
+    // STL : priority_queue, queue, list
+    /*
+    1. priority queue
+    */
+    priority_queue<int> pq;
+    pq.push(1);
+    pq.push(2);
+    pq.push(10);
+    pq.push(8);
+    pq.push(5);
+    pq.push(8);
+    
+
+
+
+    // BFS/DFS
+
+    // BINARY SEARCH
+
+}
